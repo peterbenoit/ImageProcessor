44c9c90e375032d45880d4f86e68715ba30f485d
Pete Benoit <peterbenoit@gmail.com>
2024-09-09 16:07:29 -0400

    Update ImageProcessor.js
    - a lot of adjustments for sensitive images, since they're being treated like a special case. I'm not happy with how it's turning out, most specifically adding a watermark to the blurred image and not to the clear image.
    
    https://codepen.io/peterbenoit/pen/RwzvjyY

--- ImageProcessor.js
+++ ImageProcessor.js
@@ -137,48 +137,40 @@ class ImageProcessor {
         };
     }
 
-    processSensitiveImage(container, imageUrl, options = {}) {
-        const config = { ...this.config, ...options };
+	processSensitiveImage(container, imageUrl, options = {}) {
+		const config = { ...this.config, ...options };
+		const originalClasses = container.className;
+		const handleClick = function (event) {
+			const currentImage = event.target;
+			const originalSrc = currentImage.getAttribute('data-original');
 
-        const blurredImg = document.createElement('img');
-        blurredImg.src = imageUrl;
-        blurredImg.style.filter = 'blur(50px)';
-        blurredImg.style.cursor = 'pointer';
-        blurredImg.alt = 'Sensitive Image - Click to view';
-        blurredImg.loading = config.loading;
-        blurredImg.crossOrigin = config.crossorigin;
-        blurredImg.decoding = config.decoding;
-        blurredImg.referrerPolicy = config.referrerPolicy;
+			if (!originalSrc) return;
 
-        const clearImg = document.createElement('img');
-        clearImg.src = imageUrl;
-        clearImg.style.display = 'none';
-        clearImg.alt = 'Sensitive Image';
-        clearImg.loading = config.loading;
-        clearImg.crossOrigin = config.crossorigin;
-        clearImg.decoding = config.decoding;
-        clearImg.referrerPolicy = config.referrerPolicy;
+			if (currentImage.src === originalSrc) {
+				this.processImageURL(imageUrl, {
+					...config,
+					blur: 'blur(50px)',
+					dataOriginal: originalSrc,
+					onClick: handleClick.bind(this),
+				}, currentImage);
+				currentImage.parentNode.classList.add('blurred');
+			} else {
+				currentImage.src = originalSrc;
+				currentImage.parentNode.classList.remove('blurred');
+			}
+		}.bind(this);
 
-        const overlay = document.createElement('div');
-        overlay.className = 'overlay';
-        overlay.innerHTML = `<i class="fa-light fa-eye-slash"></i> Sensitive Image`;
+		const blurredOptions = {
+			...config,
+			blur: 'blur(50px)',
+			dataOriginal: imageUrl,
+			onClick: handleClick,
+			wrapperTag: 'div',
+			wrapperClassList: `${originalClasses} blurred`
+		};
 
-        container.appendChild(blurredImg);
-        container.appendChild(clearImg);
-        container.appendChild(overlay);
-
-        blurredImg.addEventListener('click', () => {
-            blurredImg.style.display = 'none';
-            clearImg.style.display = 'block';
-            overlay.style.display = 'none';
-        });
-
-        clearImg.addEventListener('click', () => {
-            clearImg.style.display = 'none';
-            blurredImg.style.display = 'block';
-            overlay.style.display = 'flex';
-        });
-    }
+		this.processImageURL(imageUrl, blurredOptions, container);
+	}
 
     processCustomImages(images, options) {
         if (typeof images === 'string') {
@@ -188,186 +180,244 @@ class ImageProcessor {
         }
     }
 
-    processImageURL(imageUrl, options = {}, element = null) {
-        const {
-            width,
-            height,
-            cropX,
-            cropY,
-            cropWidth,
-            cropHeight,
-            grayscale,
-            invert,
-            sepia,
-            brightness,
-            contrast,
-            blur,
-            saturation,
-            hueRotate,
-            opacity,
-            rotate,
-            targetElement = element,
-            altText,
-            style,
-            loading,
-            crossorigin,
-            decoding,
-            referrerPolicy,
-            srcset,
-            sizes,
-            outputFormat,
-            quality,
-            watermark,
-            watermarkPosition,
-            watermarkRepeat,
-            watermarkAngle,
-            watermarkStyle,
-            onProcessingStart,
-            onProcessed,
-            onError,
-            onClick,
-            onHover
-        } = { ...this.config, ...options };
+	processImageURL(imageUrl, options = {}, element = null) {
+		const {
+			width,
+			height,
+			cropX,
+			cropY,
+			cropWidth,
+			cropHeight,
+			grayscale,
+			invert,
+			sepia,
+			brightness,
+			contrast,
+			blur,
+			saturation,
+			hueRotate,
+			opacity,
+			rotate,
+			targetElement = element,
+			altText,
+			style,
+			loading,
+			crossorigin,
+			decoding,
+			referrerPolicy,
+			srcset,
+			sizes,
+			outputFormat,
+			quality,
+			watermark,
+			watermarkPosition,
+			watermarkRepeat,
+			watermarkAngle,
+			watermarkStyle,
+			watermarkBackgroundColor,
+			watermarkIcon,
+			onProcessingStart = () => {},
+			onProcessed = () => {},
+			onError = () => {},
+			onClick,
+			onHover,
+			dataOriginal,
+			wrapperTag, // New option for wrapping tag
+			wrapperClassList = '' // New option for wrapper class list
+		} = { ...this.config, ...options };
 
-        onProcessingStart({ imageUrl, targetElement });
+		onProcessingStart({ imageUrl, targetElement });
 
-        if (!targetElement || !targetElement.parentNode) {
-            onError({ imageUrl, error: 'Target element not found', targetElement });
-            console.error(`Target element not found or does not exist for URL: ${imageUrl}`);
-            return;
-        }
+		if (!targetElement || !targetElement.parentNode) {
+			onError({ imageUrl, error: 'Target element not found', targetElement });
+			console.error(`Target element not found or does not exist for URL: ${imageUrl}`);
+			return;
+		}
 
-        const img = new Image();
-        img.crossOrigin = crossorigin;
-        img.loading = loading;
-        img.decoding = decoding;
-        img.referrerPolicy = referrerPolicy;
-        img.srcset = srcset;
-        img.sizes = sizes;
+		const img = new Image();
+		img.crossOrigin = crossorigin;
+		img.loading = loading;
+		img.decoding = decoding;
+		img.referrerPolicy = referrerPolicy;
 
-        img.onerror = () => {
-            onError({ imageUrl, error: 'Failed to load image', targetElement });
-            console.error(`Failed to load image from URL: ${imageUrl}`);
-        };
+		if (dataOriginal) {
+			img.setAttribute('data-original', dataOriginal);
+		}
 
-        img.onload = () => {
-            const finalWidth = width || img.width;
-            const finalHeight = height || img.height;
+		img.onerror = () => {
+			onError({ imageUrl, error: 'Failed to load image', targetElement });
+			console.error(`Failed to load image from URL: ${imageUrl}`);
+		};
 
-            const canvas = document.createElement('canvas');
-            const ctx = canvas.getContext('2d');
+		img.onload = () => {
+			const finalWidth = width || img.width;
+			const finalHeight = height || img.height;
 
-            canvas.width = finalWidth;
-            canvas.height = finalHeight;
+			const canvas = document.createElement('canvas');
+			const ctx = canvas.getContext('2d');
 
-            if (rotate) {
-                ctx.translate(finalWidth / 2, finalHeight / 2);
-                ctx.rotate((rotate * Math.PI) / 180);
-                ctx.translate(-finalWidth / 2, -finalHeight / 2);
-            }
+			canvas.width = finalWidth;
+			canvas.height = finalHeight;
 
-            ctx.filter = `
-                ${grayscale}
-                ${sepia}
-                ${invert}
-                ${brightness}
-                ${contrast}
-                ${blur}
-                ${saturation}
-                ${hueRotate}
-                ${opacity}
-            `.trim();
+			if (rotate) {
+				ctx.translate(finalWidth / 2, finalHeight / 2);
+				ctx.rotate((rotate * Math.PI) / 180);
+				ctx.translate(-finalWidth / 2, -finalHeight / 2);
+			}
 
-            ctx.drawImage(
-                img,
-                cropX,
-                cropY,
-                cropWidth || img.width,
-                cropHeight || img.height,
-                0,
-                0,
-                finalWidth,
-                finalHeight
-            );
+			ctx.filter = `
+				${grayscale}
+				${sepia}
+				${invert}
+				${brightness}
+				${contrast}
+				${blur}
+				${saturation}
+				${hueRotate}
+				${opacity}
+			`.trim();
 
-            if (watermark) {
-                if (watermark.startsWith('http')) {
-                    const watermarkImg = new Image();
-                    watermarkImg.crossOrigin = 'anonymous';
-                    watermarkImg.onload = () => {
-                        this.drawWatermarkImage(ctx, watermarkImg, finalWidth, finalHeight, watermarkPosition, watermarkRepeat);
-                        this.finalizeImage(ctx, outputFormat, quality, targetElement, style, altText, loading, crossorigin, decoding, referrerPolicy, srcset, sizes, onClick, onHover, onProcessed, imageUrl);
-                    };
-                    watermarkImg.src = watermark;
-                } else {
-                    this.drawWatermarkText(ctx, watermark, finalWidth, finalHeight, watermarkPosition, watermarkStyle, watermarkRepeat, watermarkAngle);
-                    this.finalizeImage(ctx, outputFormat, quality, targetElement, style, altText, loading, crossorigin, decoding, referrerPolicy, srcset, sizes, onClick, onHover, onProcessed, imageUrl);
-                }
-            } else {
-                this.finalizeImage(ctx, outputFormat, quality, targetElement, style, altText, loading, crossorigin, decoding, referrerPolicy, srcset, sizes, onClick, onHover, onProcessed, imageUrl);
-            }
-        };
+			ctx.drawImage(
+				img,
+				cropX,
+				cropY,
+				cropWidth || img.width,
+				cropHeight || img.height,
+				0,
+				0,
+				finalWidth,
+				finalHeight
+			);
 
-        img.src = imageUrl;
-    }
+			if (watermark) {
+				if (watermark.startsWith('http')) {
+					const watermarkImg = new Image();
+					watermarkImg.crossOrigin = 'anonymous';
+					watermarkImg.onload = () => {
+						this.drawWatermarkImage(ctx, watermarkImg, finalWidth, finalHeight, watermarkPosition, watermarkRepeat, watermarkBackgroundColor, watermarkIcon);
+						this.finalizeImage(ctx, outputFormat, quality, targetElement, style, altText, loading, crossorigin, decoding, referrerPolicy, srcset, sizes, onClick, onHover, onProcessed, imageUrl, dataOriginal, wrapperTag, wrapperClassList);
+					};
+					watermarkImg.src = watermark;
+				} else {
+					this.drawWatermarkText(ctx, watermark, finalWidth, finalHeight, watermarkPosition, watermarkStyle, watermarkRepeat, watermarkAngle, watermarkBackgroundColor, watermarkIcon);
+					this.finalizeImage(ctx, outputFormat, quality, targetElement, style, altText, loading, crossorigin, decoding, referrerPolicy, srcset, sizes, onClick, onHover, onProcessed, imageUrl, dataOriginal, wrapperTag, wrapperClassList);
+				}
+			} else {
+				this.finalizeImage(ctx, outputFormat, quality, targetElement, style, altText, loading, crossorigin, decoding, referrerPolicy, srcset, sizes, onClick, onHover, onProcessed, imageUrl, dataOriginal, wrapperTag, wrapperClassList);
+			}
+		};
 
-    drawWatermarkImage(ctx, watermarkImg, finalWidth, finalHeight, position, repeat) {
-        const pattern = ctx.createPattern(watermarkImg, repeat === 'cover' ? 'repeat' : 'no-repeat');
-        ctx.fillStyle = pattern;
-        ctx.fillRect(0, 0, finalWidth, finalHeight);
-    }
+		img.src = imageUrl;
+		img.srcset = srcset;
+		img.sizes = sizes;
+	}
 
-    drawWatermarkText(ctx, text, finalWidth, finalHeight, position, style, repeat, angle) {
-        ctx.save();
-        ctx.font = `${style.fontSize} ${style.fontFamily}`;
-        ctx.fillStyle = style.color;
-        ctx.globalAlpha = style.opacity;
+	drawWatermarkImage(ctx, watermarkImg, finalWidth, finalHeight, position, repeat, backgroundColor = 'rgba(0, 0, 0, 0.3)', icon = null) {
+		ctx.save();
+		ctx.globalAlpha = 0.5;
+		ctx.fillStyle = backgroundColor;
+		ctx.fillRect(0, 0, finalWidth, finalHeight);
+		ctx.restore();
 
-        if (repeat === 'repeat' || repeat === 'cover') {
-            ctx.translate(finalWidth / 2, finalHeight / 2);
-            ctx.rotate(angle * (Math.PI / 180));
+		const pattern = ctx.createPattern(watermarkImg, repeat === 'cover' ? 'repeat' : 'no-repeat');
+		ctx.fillStyle = pattern;
+		ctx.fillRect(0, 0, finalWidth, finalHeight);
 
-            for (let x = -finalWidth; x < finalWidth; x += 200) {
-                for (let y = -finalHeight; y < finalHeight; y += 100) {
-                    ctx.fillText(text, x, y);
-                }
-            }
+		if (icon) {
+			const iconImg = new Image();
+			iconImg.src = icon;
+			iconImg.onload = function () {
+				const iconWidth = 50;
+				const iconHeight = 50;
+				const x = position.includes('right') ? finalWidth - iconWidth - 10 : 10;
+				const y = position.includes('bottom') ? finalHeight - iconHeight - 10 : 10;
+				ctx.drawImage(iconImg, x, y, iconWidth, iconHeight);
+			};
+		}
+	}
 
-            ctx.rotate(-angle * (Math.PI / 180));
-            ctx.translate(-finalWidth / 2, -finalHeight / 2);
-        } else {
-            const x = position.includes('right') ? finalWidth - ctx.measureText(text).width - 10 : 10;
-            const y = position.includes('bottom') ? finalHeight - 10 : 30;
-            ctx.fillText(text, x, y);
-        }
+	drawWatermarkText(ctx, text, finalWidth, finalHeight, position, style, repeat, angle, backgroundColor = 'rgba(0, 0, 0, 0.3)', icon = null) {
+		ctx.save();
 
-        ctx.restore();
-    }
+		ctx.globalAlpha = 0.5;
+		ctx.fillStyle = backgroundColor;
+		ctx.fillRect(0, 0, finalWidth, finalHeight);
 
-    finalizeImage(ctx, outputFormat, quality, targetElement, style, altText, loading, crossorigin, decoding, referrerPolicy, srcset, sizes, onClick, onHover, onProcessed, imageUrl) {
-        const processedImage = new Image();
-        processedImage.src = ctx.canvas.toDataURL(outputFormat, quality);
-        processedImage.alt = altText;
-        processedImage.setAttribute('role', 'img');
-        processedImage.loading = loading;
-        processedImage.crossOrigin = crossorigin;
-        processedImage.decoding = decoding;
-        processedImage.referrerPolicy = referrerPolicy;
-        processedImage.srcset = srcset;
-        processedImage.sizes = sizes;
-        Object.assign(processedImage.style, style);
+		ctx.font = `${style.fontSize} ${style.fontFamily}`;
+		ctx.fillStyle = style.color;
+		ctx.globalAlpha = style.opacity;
 
-        if (onClick) processedImage.addEventListener('click', onClick);
-        if (onHover) processedImage.addEventListener('mouseover', onHover);
+		if (repeat === 'repeat' || repeat === 'cover') {
+			ctx.translate(finalWidth / 2, finalHeight / 2);
+			ctx.rotate(angle * (Math.PI / 180));
 
-        if (targetElement && targetElement.parentNode) {
-            targetElement.parentNode.replaceChild(processedImage, targetElement);
-        } else {
-            document.body.appendChild(processedImage);
-        }
+			for (let x = -finalWidth; x < finalWidth; x += 200) {
+				for (let y = -finalHeight; y < finalHeight; y += 100) {
+					ctx.fillText(text, x, y);
+				}
+			}
 
-        onProcessed({ imageUrl, targetElement, processedImage });
-    }
+			ctx.rotate(-angle * (Math.PI / 180));
+			ctx.translate(-finalWidth / 2, -finalHeight / 2);
+		} else {
+			const x = position.includes('right') ? finalWidth - ctx.measureText(text).width - 10 : 10;
+			const y = position.includes('bottom') ? finalHeight - 10 : 30;
+			ctx.fillText(text, x, y);
+		}
+
+		ctx.restore();
+
+		if (icon) {
+			const iconImg = new Image();
+			iconImg.src = icon;
+			iconImg.onload = function () {
+				const iconWidth = 50;
+				const iconHeight = 50;
+				const x = position.includes('right') ? finalWidth - iconWidth - 10 : 10;
+				const y = position.includes('bottom') ? finalHeight - iconHeight - 10 : 10;
+				ctx.drawImage(iconImg, x, y, iconWidth, iconHeight);
+			};
+		}
+	}
+
+	finalizeImage(ctx, outputFormat, quality, targetElement, style, altText, loading, crossorigin, decoding, referrerPolicy, srcset, sizes, onClick, onHover, onProcessed, imageUrl, dataOriginal, wrapperTag, wrapperClassList) {
+		const processedImage = new Image();
+		processedImage.src = ctx.canvas.toDataURL(outputFormat, quality);
+		processedImage.alt = altText;
+		processedImage.setAttribute('role', 'img');
+		processedImage.loading = loading;
+		processedImage.crossOrigin = crossorigin;
+		processedImage.decoding = decoding;
+		processedImage.referrerPolicy = referrerPolicy;
+		processedImage.srcset = srcset;
+		processedImage.sizes = sizes;
+
+		if (dataOriginal) {
+			processedImage.setAttribute('data-original', dataOriginal);
+		}
+
+		Object.assign(processedImage.style, style);
+
+		if (onClick) processedImage.addEventListener('click', onClick);
+		if (onHover) processedImage.addEventListener('mouseover', onHover);
+
+		let wrapperElement;
+		if (wrapperTag) {
+			wrapperElement = document.createElement(wrapperTag);
+			if (wrapperClassList) {
+				wrapperElement.className = wrapperClassList;
+			}
+			wrapperElement.appendChild(processedImage);
+		}
+
+		if (targetElement && targetElement.parentNode) {
+			targetElement.parentNode.replaceChild(wrapperElement || processedImage, targetElement);
+		} else {
+			document.body.appendChild(wrapperElement || processedImage);
+		}
+
+		onProcessed({ imageUrl, targetElement, processedImage });
+	}
 
     processImage(element, imageUrl, options) {
         this.processImageURL(imageUrl, options, element);
